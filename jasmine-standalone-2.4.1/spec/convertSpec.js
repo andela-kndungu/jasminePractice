6558bdcc2f245e5968eefc0d3212e9d36fa3a52f
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-03-13 21:45:36 +0300

    Now able to spy on functions

--- jasmine-standalone-2.4.1/spec/convertSpec.js
+++ jasmine-standalone-2.4.1/spec/convertSpec.js
@@ -3,10 +3,35 @@ describe("Convert library", function() {
   // Object through which functions will be accessed
   var convert;
 
+  // For testing spies
+  var foo;
+
   // Initialise object before running specs
   beforeAll(function() {
 
     convert = new Convert();
+
+    // Will be spied on
+    foo = {
+      isItCalled: function() {
+        var a = 'I want to be called';
+      },
+      called5times: function() {
+        var b = 'I want to be called five times';
+      }
+    };
+
+    // Start spyin
+    spyOn(foo, 'isItCalled');
+    spyOn(foo, 'called5times');
+
+    // For the test to pass
+    for (var i = 0; i < 5; i++) {
+
+      foo.called5times();
+    }
+
+    foo.isItCalled();
   });
 
   // The specs, one for each function in the object
@@ -29,5 +54,18 @@ describe("Convert library", function() {
 
     pending('Yet to implement the function');
     expect(convert.lyToKm(1)).toEqual(9.461e+12);
+  });
+
+  describe('Calls all the necessary functions', function() {
+
+    it('Calls a function', function() {
+
+      expect(foo.isItCalled).toHaveBeenCalled();
+    });
+
+    it('Calls a function five times', function() {
+
+      expect(foo.called5times).toHaveBeenCalledTimes(5);
+    });
   });
 });
